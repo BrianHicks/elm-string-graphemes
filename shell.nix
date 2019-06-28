6f922c78e490357312321e284fe676c30951dff5
Brian Hicks <brian@brianthicks.com>
2019-06-27 22:01:12 -0500

    set up a basic elm project

---
+++ shell.nix
@@ -0,0 +1,16 @@
+with import (builtins.fetchTarball rec {
+  # grab a hash from here: https://nixos.org/channels/
+  name = "nixpkgs-darwin-18.09pre154167.3d4459e31bd";
+  url = "https://github.com/nixos/nixpkgs/archive/3d4459e31bdccfb581e27dfffbec44d62d121349.tar.gz";
+  # Hash obtained using `nix-prefetch-url --unpack <url>`
+  sha256 = "08pzpwxjrf8p7z0hcw5nhwrm6rw180g5446aandl41zvqvdjhigb";
+}) {};
+
+stdenv.mkDerivation {
+  name = "elm-unicode-segmentation";
+  buildInputs = [
+    git
+    elmPackages.elm
+    elmPackages.elm-format
+  ];
+}
