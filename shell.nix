be0331c72a4fd839fa7ba711bbe958f4f7cac0b3
Brian Hicks <brian@brianthicks.com>
2019-06-27 23:33:41 -0500

    add elm-test

--- shell.nix
+++ shell.nix
@@ -6,13 +6,22 @@ with import (builtins.fetchTarball rec {
   sha256 = "08pzpwxjrf8p7z0hcw5nhwrm6rw180g5446aandl41zvqvdjhigb";
 }) {};
 
-stdenv.mkDerivation {
-  name = "elm-unicode-segmentation";
-  buildInputs = [
-    elmPackages.elm
-    elmPackages.elm-format
-    git
-    gnumake
-    python37
-  ];
-}
+let
+  elmTools = import (pkgs.fetchFromGitHub {
+    owner = "turboMaCk";
+    repo = "nix-elm-tools";
+    rev = "ecfb53b289a5d2cdbf09579f9ba78a335dd857ca";
+    sha256 = "1fbh6cjq93zc7li89llh9gr0hhxz8kdf6wyld8wnay5d4d8z319k";
+  }) { inherit pkgs; };
+in
+  stdenv.mkDerivation {
+    name = "elm-unicode-segmentation";
+    buildInputs = [
+      elmPackages.elm
+      elmPackages.elm-format
+      elmTools.elm-test
+      git
+      gnumake
+      python37
+    ];
+  }
