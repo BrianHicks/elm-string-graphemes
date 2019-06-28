ab496fd81c74aa79b6ede1df3e3af55c119a5a97
Brian Hicks <brian@brianthicks.com>
2019-06-28 09:26:42 -0500

    add another test for LFCR

--- tests/String/SegmentationSpec.elm
+++ tests/String/SegmentationSpec.elm
@@ -8,9 +8,16 @@ import Test exposing (..)
 graphemesSpec : Test
 graphemesSpec =
     describe "graphemes"
-        [ test "breaks on CRLF" <| \_ -> expectIdentity "\u{000D}\n"
-        , test "breaks on CR" <| \_ -> expectIdentity "\u{000D}"
-        , test "breaks on LF" <| \_ -> expectIdentity "\n"
+        [ describe "line breaks get broken"
+            [ test "breaks on CRLF" <| \_ -> expectIdentity "\u{000D}\n"
+            , test "breaks on CR" <| \_ -> expectIdentity "\u{000D}"
+            , test "breaks on LF" <| \_ -> expectIdentity "\n"
+            , test "LFCR is two characters" <|
+                \_ ->
+                    "\n\u{000D}"
+                        |> Segmentation.graphemes
+                        |> Expect.equal (Ok [ "\n", "\u{000D}" ])
+            ]
         ]
 
 
