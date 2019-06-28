e0ab000d6cf0d2294e6400d92ef3741239c030fb
Brian Hicks <brian@brianthicks.com>
2019-06-28 11:12:00 -0500

    test control characters always segment

--- src/String/Segmentation.elm
+++ src/String/Segmentation.elm
@@ -2,6 +2,7 @@ module String.Segmentation exposing (graphemes)
 
 import Parser exposing ((|=), Parser, Step(..), loop)
 import String.Segmentation.CR as CR
+import String.Segmentation.Control as Control
 import String.Segmentation.LF as LF
 
 
@@ -16,6 +17,7 @@ graphemesLoop current =
         [ cons current (Parser.token "\u{000D}\n")
         , cons current CR.parser
         , cons current LF.parser
+        , cons current Control.parser
         , Parser.map (\_ -> Done (List.reverse current)) Parser.end
         ]
 
--- tests/String/SegmentationSpec.elm
+++ tests/String/SegmentationSpec.elm
@@ -1,6 +1,8 @@
 module String.SegmentationSpec exposing (graphemesSpec)
 
+import Data.Control as Control
 import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer)
 import String.Segmentation as Segmentation
 import Test exposing (..)
 
@@ -18,9 +20,23 @@ graphemesSpec =
                         |> Segmentation.graphemes
                         |> Expect.equal (Ok [ "\n", "\u{000D}" ])
             ]
+        , describe "control characters"
+            [ fuzz2 controlCharacter controlCharacter "always form new segments" <|
+                \c1 c2 ->
+                    (c1 ++ c2)
+                        |> Segmentation.graphemes
+                        |> Expect.equal (Ok [ c1, c2 ])
+            ]
         ]
 
 
 expectIdentity : String -> Expectation
 expectIdentity s =
     Expect.equal (Ok [ s ]) (Segmentation.graphemes s)
+
+
+controlCharacter : Fuzzer String
+controlCharacter =
+    Control.chars
+        |> List.map (Fuzz.constant << String.fromList << List.singleton)
+        |> Fuzz.oneOf
