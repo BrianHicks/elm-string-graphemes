2bab48984d986cfcabe8b97258c9b6b71ada6c55
Brian Hicks <brian@brianthicks.com>
2019-06-28 09:20:40 -0500

    split on CR and LF

--- src/String/Segmentation.elm
+++ src/String/Segmentation.elm
@@ -1,6 +1,8 @@
 module String.Segmentation exposing (graphemes)
 
 import Parser exposing ((|=), Parser, Step(..), loop)
+import String.Segmentation.CR as CR
+import String.Segmentation.LF as LF
 
 
 graphemes : String -> Result (List Parser.DeadEnd) (List String)
@@ -12,6 +14,8 @@ graphemesLoop : List String -> Parser (Step (List String) (List String))
 graphemesLoop current =
     Parser.oneOf
         [ cons current (Parser.token "\u{000D}\n")
+        , cons current CR.parser
+        , cons current LF.parser
         , Parser.map (\_ -> Done (List.reverse current)) Parser.end
         ]
 
