e781bb06004f1cb6b65d85c7101ccf847f518ac9
Brian Hicks <brian@brianthicks.com>
2019-07-01 10:14:37 -0500

    use a test input that actually requires segmentation

--- size-benchmarks/src/WithSegmentation.elm
+++ size-benchmarks/src/WithSegmentation.elm
@@ -1,13 +1,13 @@
 module WithSegmentation exposing (main)
 
 import Html exposing (Html)
-import String.Graphemes as Segmentation
+import String.Graphemes as Graphemes
 
 
 main : Html msg
 main =
-    ""
-        |> Segmentation.graphemes
+    "\u{1F9B8}\u{1F3FD}\u{200D}♂️"
+        |> Graphemes.graphemes
         |> Result.withDefault []
         |> String.concat
         |> Html.text
--- size-benchmarks/src/WithoutSegmentation.elm
+++ size-benchmarks/src/WithoutSegmentation.elm
@@ -5,7 +5,7 @@ import Html exposing (Html)
 
 main : Html msg
 main =
-    ""
+    "\u{1F9B8}\u{1F3FD}\u{200D}♂️"
         |> String.toList
         |> String.fromList
         |> Html.text
