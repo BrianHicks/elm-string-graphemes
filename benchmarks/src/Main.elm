77036c0c62b2e81b923ec8f35149e5316f9b4948
Brian Hicks <brian@brianthicks.com>
2019-07-03 15:13:14 -0500

    benchmark Parser.run vs String.foldl

--- benchmarks/src/Main.elm
+++ benchmarks/src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (main)
 
 import Benchmark exposing (..)
 import Benchmark.Runner exposing (BenchmarkProgram, program)
+import Parser
 import String.Graphemes as Segmentation
 
 
@@ -19,4 +20,20 @@ main =
                 (\_ -> String.toList "\u{1F9B8}\u{1F3FD}\u{200D}♂️")
                 "graphemes"
                 (\_ -> Segmentation.toList "\u{1F9B8}\u{1F3FD}\u{200D}♂️")
+            , Benchmark.compare "foldls"
+                "parser"
+                (\_ ->
+                    Parser.run
+                        (Parser.loop []
+                            (\state ->
+                                Parser.oneOf
+                                    [ Parser.map (\_ -> Parser.Done state) Parser.end
+                                    , Parser.map (\c -> Parser.Loop (c :: state)) (Parser.getChompedString (Parser.chompIf (\_ -> True)))
+                                    ]
+                            )
+                        )
+                        "1234567890"
+                )
+                "foldl"
+                (\_ -> String.foldl (::) [] "1234567890")
             ]
