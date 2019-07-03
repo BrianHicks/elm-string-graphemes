d998fabcbb9567da20ddcbfdc74c2bc9477029f6
Brian Hicks <brian@brianthicks.com>
2019-07-03 17:17:34 -0500

    benchmark the public API

--- benchmarks/src/Main.elm
+++ benchmarks/src/Main.elm
@@ -3,8 +3,7 @@ module Main exposing (main)
 import Benchmark exposing (..)
 import Benchmark.Runner exposing (BenchmarkProgram, program)
 import Parser
-import String.Graphemes as Segmentation
-import String.Graphemes.Parser as Parser
+import String.Graphemes as Graphemes
 
 
 main : BenchmarkProgram
@@ -14,11 +13,11 @@ main =
             [ Benchmark.compare "plain text"
                 "characters"
                 (\_ -> String.toList "123456")
-                "parser"
-                (\_ -> Parser.foldl (::) [] "123456" |> List.reverse)
+                "graphemes"
+                (\_ -> Graphemes.toList "123456")
             , Benchmark.compare "emoji"
                 "characters"
                 (\_ -> String.toList "\u{1F9B8}\u{1F3FD}\u{200D}♂️")
-                "parser"
-                (\_ -> Parser.foldl (::) [] "\u{1F9B8}\u{1F3FD}\u{200D}♂️" |> List.reverse)
+                "graphemes"
+                (\_ -> Graphemes.toList "\u{1F9B8}\u{1F3FD}\u{200D}♂️")
             ]
