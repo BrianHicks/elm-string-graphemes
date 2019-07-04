80681c11876dba43e957022bc00b6b2a462294b7
Brian Hicks <brian@brianthicks.com>
2019-07-03 20:37:37 -0500

    copy and modify core's string

--- src/String/Graphemes.elm
+++ src/String/Graphemes.elm
@@ -1,31 +1,78 @@
 module String.Graphemes exposing
     ( isEmpty, length, reverse, repeat, replace
-    , append, concat, split, join
-    , toList
+    , append, concat, split, join, words, lines
+    , slice, left, right, dropLeft, dropRight
+    , contains, startsWith, endsWith, indexes, indices
+    , toInt, fromInt
+    , toFloat, fromFloat
+    , fromChar, cons, uncons
+    , toList, fromList
+    , toUpper, toLower, pad, padLeft, padRight, trim, trimLeft, trimRight
+    , map, filter, foldl, foldr, any, all
     )
 
-{-| The goal of this API is to recreate `elm/core`'s `String` module as
-faithfully as possible, but to operate on grapheme boundaries instead of bytes
-or characters. This can smooth out some edge cases when processing user-entered
-text: emoji, for example, will not be split awkwardly. Basically, if you're
-dealing with text that the user sees, work at the grapheme level. See the
-package README for more information on motivation.
+{-| A built-in representation for efficient string manipulation. String literals
+are enclosed in `"double quotes"`. Strings are _not_ lists of characters.
 
-@docs isEmpty, length, reverse, repeat, replace
+
+# Strings
+
+@docs String, isEmpty, length, reverse, repeat, replace
 
 
 # Building and Splitting
 
-@docs append, concat, split, join
+@docs append, concat, split, join, words, lines
+
+
+# Get Substrings
+
+@docs slice, left, right, dropLeft, dropRight
+
+
+# Check for Substrings
+
+@docs contains, startsWith, endsWith, indexes, indices
+
+
+# Int Conversions
+
+@docs toInt, fromInt
+
+
+# Float Conversions
+
+@docs toFloat, fromFloat
+
+
+# Char Conversions
+
+@docs fromChar, cons, uncons
 
 
 # List Conversions
 
-@docs toList
+@docs toList, fromList
+
+
+# Formatting
+
+Cosmetic operations such as padding with extra characters or trimming whitespace.
+
+@docs toUpper, toLower, pad, padLeft, padRight, trim, trimLeft, trimRight
+
+
+# Higher-Order Functions
+
+@docs map, filter, foldl, foldr, any, all
 
 -}
 
 import String.Graphemes.Parser as Parser
+
+
+
+-- STRINGS
 
 
 {-| Determine if a string is empty.
@@ -63,10 +110,10 @@ length =
 -}
 reverse : String -> String
 reverse =
-    foldl (::) [] >> String.join ""
+    toList >> List.reverse >> concat
 
 
-{-| Repeat a string n times.
+{-| Repeat a string _n_ times.
 
     repeat 3 "ha" --> "hahaha"
 
@@ -83,12 +130,10 @@ repeat =
     replace "," "/" "a,b,c,d,e" --> "a/b/c/d/e"
 
 **Note:** If you need more advanced replacements, check out the
-[elm/parser](https://package.elm-lang.org/packages/elm/parser/latest) or
-[elm/regex](https://package.elm-lang.org/packages/elm/regex/latest) package.
+[`elm/parser`][parser] or [`elm/regex`][regex] package.
 
-**Second note:** you might expect us to do something special with graphemes
-here. Nope! This transformation is extremely literal. If you have a use case
-where that causes problems, please open an issue.
+[parser]: /packages/elm/parser/latest
+[regex]: /packages/elm/regex/latest
 
 -}
 replace : String -> String -> String -> String
@@ -96,9 +141,12 @@ replace =
     String.replace
 
 
-{-| Append two strings. You can also use [the `(++)`
-operator](https://package.elm-lang.org/packages/elm/core/latest/Basics#++) to do
-this.
+
+-- BUILDING AND SPLITTING
+
+
+{-| Append two strings. You can also use [the `(++)` operator](Basics#++)
+to do this.
 
     append "butter" "fly" --> "butterfly"
 
@@ -120,9 +168,9 @@ concat =
 
 {-| Split a string using a given separator.
 
-    split "," "cat,dog,cow" --> ["cat","dog","cow"]
+    split "," "cat,dog,cow" --> [ "cat", "dog", "cow" ]
 
-    split "/" "home/evan/Desktop/" --> ["home","evan","Desktop", ""]
+    split "/" "home/evan/Desktop/" --> [ "home", "evan", "Desktop", "" ]
 
 -}
 split : String -> String -> List String
@@ -144,35 +192,450 @@ join =
     String.join
 
 
+{-| Break a string into words, splitting on chunks of whitespace.
+
+    words "How are \t you? \n Good?" --> [ "How", "are", "you?", "Good?" ]
+
+-}
+words : String -> List String
+words =
+    String.words
+
+
+{-| Break a string into lines, splitting on newlines.
+
+    lines "How are you?\nGood?" --> [ "How are you?", "Good?" ]
+
+-}
+lines : String -> List String
+lines =
+    String.lines
+
+
+
+-- SUBSTRINGS
+
+
+{-| Take a substring given a start and end index. Negative indexes
+are taken starting from the _end_ of the list.
+
+    slice 7 9 "snakes on a plane!" --> "on"
+
+    slice 0 6 "snakes on a plane!" --> "snakes"
+
+    slice 0 -7 "snakes on a plane!" --> "snakes on a"
+
+    slice -6 -1 "snakes on a plane!" --> "plane"
+
+-}
+slice : Int -> Int -> String -> String
+slice =
+    String.slice
+
+
+{-| Take _n_ characters from the left side of a string.
+
+    left 2 "Mulder" --> "Mu"
+
+-}
+left : Int -> String -> String
+left =
+    String.left
+
+
+{-| Take _n_ characters from the right side of a string.
+
+    right 2 "Scully" --> "ly"
+
+-}
+right : Int -> String -> String
+right =
+    String.right
+
+
+{-| Drop _n_ characters from the left side of a string.
+
+    dropLeft 2 "The Lone Gunmen" --> "e Lone Gunmen"
+
+-}
+dropLeft : Int -> String -> String
+dropLeft =
+    String.dropLeft
+
+
+{-| Drop _n_ characters from the right side of a string.
+
+    dropRight 2 "Cigarette Smoking Man" --> "Cigarette Smoking M"
+
+-}
+dropRight : Int -> String -> String
+dropRight =
+    String.dropRight
+
+
+
+-- DETECT SUBSTRINGS
+
+
+{-| See if the second string contains the first one.
+
+    contains "the" "theory" --> True
+
+    contains "hat" "theory" --> False
+
+    contains "THE" "theory" --> False
+
+-}
+contains : String -> String -> Bool
+contains =
+    String.contains
+
+
+{-| See if the second string starts with the first one.
+
+    startsWith "the" "theory" --> True
+
+    startsWith "ory" "theory" --> False
+
+-}
+startsWith : String -> String -> Bool
+startsWith =
+    String.startsWith
+
+
+{-| See if the second string ends with the first one.
+
+    endsWith "the" "theory" --> False
+
+    endsWith "ory" "theory" --> True
+
+-}
+endsWith : String -> String -> Bool
+endsWith =
+    String.endsWith
+
+
+{-| Get all of the indexes for a substring in another string.
+
+    indexes "i" "Mississippi" --> [ 1, 4, 7, 10 ]
+
+    indexes "ss" "Mississippi" --> [ 2, 5 ]
+
+    indexes "needle" "haystack" --> []
+
+-}
+indexes : String -> String -> List Int
+indexes =
+    String.indexes
+
+
+{-| Alias for `indexes`.
+-}
+indices : String -> String -> List Int
+indices =
+    String.indexes
+
+
+
+-- FORMATTING
+
+
+{-| Convert a string to all upper case. Useful for case-insensitive comparisons
+and VIRTUAL YELLING.
+
+    toUpper "skinner" --> "SKINNER"
+
+-}
+toUpper : String -> String
+toUpper =
+    String.toUpper
+
+
+{-| Convert a string to all lower case. Useful for case-insensitive comparisons.
+
+    toLower "X-FILES" --> "x-files"
+
+-}
+toLower : String -> String
+toLower =
+    String.toLower
+
+
+{-| Pad a string on both sides until it has a given length.
+
+    pad 5 ' ' "1" --> "  1  "
+
+    pad 5 ' ' "11" --> "  11 "
+
+    pad 5 ' ' "121" --> " 121 "
+
+-}
+pad : Int -> Char -> String -> String
+pad =
+    String.pad
+
+
+{-| Pad a string on the left until it has a given length.
+
+    padLeft 5 '.' "1" --> "....1"
+
+    padLeft 5 '.' "11" --> "...11"
+
+    padLeft 5 '.' "121" --> "..121"
+
+-}
+padLeft : Int -> Char -> String -> String
+padLeft =
+    String.padLeft
+
+
+{-| Pad a string on the right until it has a given length.
+
+    padRight 5 '.' "1" --> "1...."
+
+    padRight 5 '.' "11" --> "11..."
+
+    padRight 5 '.' "121" --> "121.."
+
+-}
+padRight : Int -> Char -> String -> String
+padRight =
+    String.padRight
+
+
+{-| Get rid of whitespace on both sides of a string.
+
+    trim "  hats  \n" --> "hats"
+
+-}
+trim : String -> String
+trim =
+    String.trim
+
+
+{-| Get rid of whitespace on the left of a string.
+
+    trimLeft "  hats  \n" --> "hats  \n"
+
+-}
+trimLeft : String -> String
+trimLeft =
+    String.trimLeft
+
+
+{-| Get rid of whitespace on the right of a string.
+
+    trimRight "  hats  \n" --> "  hats"
+
+-}
+trimRight : String -> String
+trimRight =
+    String.trimRight
+
+
+
+-- INT CONVERSIONS
+
+
+{-| Try to convert a string into an int, failing on improperly formatted strings.
+
+    String.toInt "123" --> Just 123
+
+    String.toInt "-42" --> Just -42
+
+    String.toInt "3.1" --> Nothing
+
+    String.toInt "31a" --> Nothing
+
+If you are extracting a number from some raw user input, you will typically
+want to use [`Maybe.withDefault`](Maybe#withDefault) to handle bad data:
+
+    Maybe.withDefault 0 (String.toInt "42") --> 42
+
+    Maybe.withDefault 0 (String.toInt "ab") --> 0
+
+-}
+toInt : String -> Maybe Int
+toInt =
+    String.toInt
+
+
+{-| Convert an `Int` to a `String`.
+
+    String.fromInt 123 --> "123"
+
+    String.fromInt -42 --> "-42"
+
+Check out [`Debug.toString`](Debug#toString) to convert _any_ value to a string
+for debugging purposes.
+
+-}
+fromInt : Int -> String
+fromInt =
+    String.fromInt
+
+
+
+-- FLOAT CONVERSIONS
+
+
+{-| Try to convert a string into a float, failing on improperly formatted strings.
+-}
+toFloat : String -> Maybe Float
+toFloat =
+    String.toFloat
+
+
+{-| Convert a `Float` to a `String`.
+
+    String.fromFloat 123 --> "123"
+
+    String.fromFloat -42 --> "-42"
+
+    String.fromFloat 3.9 --> "3.9"
+
+Check out [`Debug.toString`](Debug#toString) to convert _any_ value to a string
+for debugging purposes.
+
+-}
+fromFloat : Float -> String
+fromFloat =
+    String.fromFloat
+
+
+
+-- LIST CONVERSIONS
+
+
 {-| Convert a string to a list of characters.
 
-    toList "abc" --> ["a","b","c"]
+    toList "abc" --> [ "a", "b", "c" ]
 
-    toList "🙈🙉🙊" --> ["🙈","🙉","🙊"]
-
-**Note:** this is different from `String`! Graphemes can be more than one
-character wide, so we have to return `String`s here instead of `Char`s.
+    toList "🙈🙉🙊" --> [ "🙈", "🙉", "🙊" ]
 
 -}
 toList : String -> List String
 toList =
-    foldl (::) [] >> List.reverse
+    foldr (::) []
 
 
-{-| Break a string into graphemes (the characters you percieve, as opposed to
-the bytes used to store the string or the codepoints used to make the
-character.)
+{-| Convert a list of characters into a String. Can be useful if you
+want to create a string primarily by consing, perhaps for decoding
+something.
 
-This will preserve things like accents, Hangul syllables, and emoji with skin
-tone.
+    fromList [ 'a', 'b', 'c' ] --> "abc"
 
-This uses the [Unicode TR29
-algorithm](http://www.unicode.org/reports/tr29/#Table_Combining_Char_Sequences_and_Grapheme_Clusters). There's
-also a [more visual description of the
-algorithm](https://www.unicode.org/Public/12.1.0/ucd/auxiliary/GraphemeBreakTest.html)
-if you're interested in learning more.
+    fromList [ '🙈', '🙉', '🙊' ] --> "🙈🙉🙊"
 
 -}
-foldl : (String -> a -> a) -> a -> String -> a
+fromList : List Char -> String
+fromList =
+    String.fromList
+
+
+
+-- CHAR CONVERSIONS
+
+
+{-| Create a string from a given character.
+
+    fromChar 'a' --> "a"
+
+-}
+fromChar : Char -> String
+fromChar =
+    String.fromChar
+
+
+{-| Add a character to the beginning of a string.
+
+    cons 'T' "he truth is out there" --> "The truth is out there"
+
+-}
+cons : Char -> String -> String
+cons =
+    String.cons
+
+
+{-| Split a non-empty string into its head and tail. This lets you
+pattern match on strings exactly as you would with lists.
+
+    uncons "abc" --> Just ( 'a', "bc" )
+
+    uncons "" --> Nothing
+
+-}
+uncons : String -> Maybe ( Char, String )
+uncons =
+    String.uncons
+
+
+
+-- HIGHER-ORDER FUNCTIONS
+
+
+{-| Transform every grapheme in a string
+-}
+map : (Char -> Char) -> String -> String
+map =
+    String.map
+
+
+{-| Keep only the characters that pass the test.
+
+    filter Char.isDigit "R2-D2" --> "22"
+
+-}
+filter : (Char -> Bool) -> String -> String
+filter =
+    String.filter
+
+
+{-| Reduce a string from the left.
+
+    foldl (++) "" "time" --> "emit"
+
+-}
+foldl : (String -> b -> b) -> b -> String -> b
 foldl =
     Parser.foldl
+
+
+{-| Reduce a string from the right.
+
+    foldr (++) "" "time" --> "time"
+
+-}
+foldr : (String -> b -> b) -> b -> String -> b
+foldr fn state string =
+    string
+        |> foldl (::) []
+        |> List.foldl fn state
+
+
+{-| Determine whether _any_ characters pass the test.
+
+    any Char.isDigit "90210" --> True
+
+    any Char.isDigit "R2-D2" --> True
+
+    any Char.isDigit "heart" --> False
+
+-}
+any : (Char -> Bool) -> String -> Bool
+any =
+    String.any
+
+
+{-| Determine whether _all_ characters pass the test.
+
+    all Char.isDigit "90210" --> True
+
+    all Char.isDigit "R2-D2" --> False
+
+    all Char.isDigit "heart" --> False
+
+-}
+all : (Char -> Bool) -> String -> Bool
+all =
+    String.all
--- tests/String/GraphemesSpec.elm
+++ tests/String/GraphemesSpec.elm
@@ -1,60 +1,118 @@
 module String.GraphemesSpec exposing (compatibilitySpec)
 
 import Expect
-import Fuzz exposing (list, string)
+import Fuzz exposing (Fuzzer, int, list, string)
 import String.Graphemes as Graphemes
 import Test exposing (..)
 
 
 compatibilitySpec : Test
 compatibilitySpec =
-    -- TODO: some of these may be slightly flakey if the fuzzer ever manages to
-    -- create a valid multi-byte sequence. Probably should test for that
-    -- explicitly or guard against it.
     describe "compatibility with elm/core String"
-        [ fuzz string "isEmpty" <|
+        [ fuzz simpleString "isEmpty" <|
             \s ->
                 Expect.equal
                     (String.isEmpty s)
                     (Graphemes.isEmpty s)
-        , fuzz string "length" <|
+        , fuzz simpleString "length" <|
             \s ->
                 Expect.equal
                     (String.length s)
                     (Graphemes.length s)
-        , fuzz string "reverse" <|
+        , fuzz simpleString "reverse" <|
             \s ->
                 Expect.equal
                     (String.reverse s)
                     (Graphemes.reverse s)
-        , fuzz2 (Fuzz.intRange -100 100) string "repeat" <|
+        , fuzz2 (Fuzz.intRange -100 100) simpleString "repeat" <|
             \i s ->
                 Expect.equal
                     (String.repeat i s)
                     (Graphemes.repeat i s)
-        , fuzz3 string string string "replace" <|
+        , fuzz3 simpleString simpleString simpleString "replace" <|
             \from to source ->
                 Expect.equal
                     (String.replace from to source)
                     (Graphemes.replace from to source)
-        , fuzz2 string string "append" <|
+        , fuzz2 simpleString simpleString "append" <|
             \a b ->
                 Expect.equal
                     (String.append a b)
                     (Graphemes.append a b)
-        , fuzz (list string) "concat" <|
+        , fuzz (list simpleString) "concat" <|
             \strings ->
                 Expect.equal
                     (String.concat strings)
                     (Graphemes.concat strings)
-        , fuzz2 string string "split" <|
+        , fuzz2 simpleString simpleString "split" <|
             \a b ->
                 Expect.equal
                     (String.split a b)
                     (Graphemes.split a b)
-        , fuzz2 string (list string) "join" <|
+        , fuzz2 simpleString (list simpleString) "join" <|
             \joiner strings ->
                 Expect.equal
                     (String.join joiner strings)
                     (Graphemes.join joiner strings)
+        , fuzz (Fuzz.map String.concat (list simpleString)) "words" <|
+            \strings ->
+                Expect.equal
+                    (String.words strings)
+                    (Graphemes.words strings)
+        , fuzz (Fuzz.map (String.join "\n") (list simpleString)) "lines" <|
+            \strings ->
+                Expect.equal
+                    (String.lines strings)
+                    (Graphemes.lines strings)
+        , fuzz3 int int simpleString "slice" <|
+            \a b string ->
+                Expect.equal
+                    (String.slice a b string)
+                    (Graphemes.slice a b string)
+        , fuzz2 int simpleString "left" <|
+            \offset string ->
+                Expect.equal
+                    (String.left offset string)
+                    (Graphemes.left offset string)
+        , fuzz2 int simpleString "right" <|
+            \offset string ->
+                Expect.equal
+                    (String.right offset string)
+                    (Graphemes.right offset string)
+
+        -- , todo "dropLeft"
+        -- , todo "dropRight"
+        -- , todo "contains"
+        -- , todo "startsWith"
+        -- , todo "endsWith"
+        -- , todo "indexes"
+        -- , todo "indices"
+        -- , todo "toUpper"
+        -- , todo "toLower"
+        -- , todo "padLeft"
+        -- , todo "padRight"
+        -- , todo "trim"
+        -- , todo "trimLeft"
+        -- , todo "trimRight"
+        -- , todo "toInt"
+        -- , todo "fromInt"
+        -- , todo "toFloat"
+        -- , todo "fromFloat"
+        -- , todo "toList"
+        -- , todo "fromList"
+        -- , todo "fromChar"
+        -- , todo "cons"
+        -- , todo "uncons"
+        -- , todo "map"
+        -- , todo "filter"
+        -- , todo "foldl"
+        -- , todo "foldr"
+        -- , todo "any"
+        -- , todo "all"
         ]
+
+
+simpleString : Fuzzer String
+simpleString =
+    Fuzz.intRange (Char.toCode 'A') (Char.toCode 'Z')
+        |> Fuzz.map (Char.fromCode >> String.fromChar)
